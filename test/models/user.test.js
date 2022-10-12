9d3218a8c852e9827df188774fcecaf6da950b6d
Zac <zac@zacmoss.co.uk>
2022-10-12 18:15:17 +0100

    passed find saved user test

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -28,4 +28,24 @@ describe("User model", () => {
       done();
     });
   });
+
+  it("can save a user", (done) => {
+    const user = new User({
+      username: "someone",
+      email: "someone@example.com",
+      password: "password",
+      peeps: [],
+    });
+
+    user.save((err) => {
+      expect(err).toBeNull();
+
+      User.find((err, users) => {
+        expect(err).toBeNull();
+        expect(users[0].email).toEqual("someone@example.com");
+        expect(users[0].password).toEqual("password");
+        done();
+      });
+    });
+  });
 });
