9672926f376a0a8951bee6df2a140f2c615930da
Zac <zac@zacmoss.co.uk>
2022-10-13 22:58:53 +0100

    if error says MongoServerError usersController.create sends a 401 status

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
 exports.create = (req, res, userModel) => {
-  new userModel(req.body).save((err, user) =>
-    res.send(JSON.stringify({ _id: user._id, username: user.username }))
-  );
+  new userModel(req.body).save((err, user) => {
+    if (err && err.toString().includes("MongoServerError"))
+      return res.status(401);
+    res.send(JSON.stringify({ _id: user._id, username: user.username }));
+  });
 };
--- test/controllers/users.test.js
+++ test/controllers/users.test.js
@@ -4,7 +4,7 @@ let res;
 
 describe("Users controller", () => {
   beforeEach(() => {
-    res = { send: jest.fn() };
+    res = { send: jest.fn(), status: jest.fn() };
   });
 
   it("create creates a new user", () => {
@@ -29,5 +29,21 @@ describe("Users controller", () => {
     expect(res.send).toHaveBeenCalledWith(
       JSON.stringify({ _id: 1, username: "username" })
     );
+  });
+
+  it("create sends a 401 status if the username or email already exist", () => {
+    const req = {
+      session: {},
+      body: { username: "username", password: "password", email: "email" },
+    };
+
+    const mockUser = jest.fn().mockImplementation(() => {
+      return {
+        save: jest.fn((callback) => callback(new Error("MongoServerError"))),
+      };
+    });
+    usersController.create(req, res, mockUser);
+    expect(mockUser).toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(401);
   });
 });
