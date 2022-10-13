7fb09c3900ebeef1972ebd4451fea193c544bd10
Zac <zac@zacmoss.co.uk>
2022-10-13 21:19:59 +0100

    refactored

--- controllers/session.js
+++ controllers/session.js
@@ -11,6 +11,8 @@ exports.create = (req, res, getEncryptedPassword, userModel = User) => {
   const encryptedPassword = getEncryptedPassword(req.body.password);
   userModel.findOne({ username: req.body.username }, (err, user) => {
     if (encryptedPassword !== user.password) return res.status(401);
+    req.session.user = user;
     res.send(JSON.stringify({ _id: user._id, username: user.username }));
+    return req.session.user;
   });
 };
