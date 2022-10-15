aa3952f195abf53db7583aa79df4d57e7c5b6300
Zac <zac@zacmoss.co.uk>
2022-10-15 09:00:17 +0100

    peepsController.index now returns latets 50 peeps in reverse chronological order

--- controllers/peeps.js
+++ controllers/peeps.js
@@ -1,1 +1,4 @@
-exports.index = (req, res) => {};
+exports.index = async (req, res, peepsModel) => {
+  const peeps = await peepsModel.find().sort({ createdAt: -1 }).limit(50);
+  res.json(peeps);
+};
--- test/controllers/peeps.test.js
+++ test/controllers/peeps.test.js
@@ -1,5 +1,4 @@
 import * as peepsController from "../../controllers/peeps";
-import { index } from "../../controllers/session";
 
 let res;
 describe("Peeps controller", () => {
@@ -10,9 +9,32 @@ describe("Peeps controller", () => {
       sendStatus: jest.fn(),
     };
   });
-  it("gets the latest 50 peeps", () => {
-    const peepsModel = { find: jest.fn() };
-    peepsController.index({}, res);
-    expect();
+  it("gets the latest 50 peeps", async () => {
+    const peepsModel = {
+      find: jest.fn().mockReturnThis(),
+      sort: jest.fn().mockReturnThis(),
+      limit: jest.fn(),
+    };
+
+    const peeps = [
+      {
+        _id: 1,
+        userId: 1,
+        body: "foo",
+        createdAt: new Date(2022, 10, 12),
+      },
+      {
+        _id: 2,
+        userId: 2,
+        body: "bar",
+        createdAt: new Date(2022, 10, 11),
+      },
+    ];
+    peepsModel.limit.mockReturnValueOnce(peeps);
+    await peepsController.index({}, res, peepsModel);
+    expect(peepsModel.find).toHaveBeenCalled();
+    expect(peepsModel.sort).toHaveBeenCalledWith({ createdAt: -1 });
+    expect(peepsModel.limit).toHaveBeenCalledWith(50);
+    expect(res.json).toHaveBeenCalledWith(peeps);
   });
 });
