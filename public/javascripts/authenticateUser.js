56af2b54faf0cf48107c193fefab1a82cd571001
Zac <zac@zacmoss.co.uk>
2022-10-17 22:58:49 +0100

    authenticateUser returns 403 if no authorization header was provided

--- public/javascripts/authenticateUser.js
+++ public/javascripts/authenticateUser.js
@@ -1,5 +1,11 @@
 authenticateUser = (req, res, next) => {
-  next();
+  if (
+    req.cookies.user_sid &&
+    req.headers.Authorization &&
+    req.headers.Authorization.split(" ")[1] === req.cookies.user_sid.toString()
+  )
+    return next();
+  res.sendStatus(403);
 };
 
 module.exports = authenticateUser;
--- test/authenticateUser.test.js
+++ test/authenticateUser.test.js
@@ -4,15 +4,33 @@ let res, next;
 
 describe("authenticateUser", () => {
   beforeEach(() => {
-    res = {};
+    res = { sendStatus: jest.fn() };
     next = jest.fn();
   });
   it("calls next when userSid matches the authorization token", () => {
     const req = {
       cookies: { user_sid: 1 },
-      headers: { authorization: "Token 1" },
+      headers: { Authorization: "Token 1" },
     };
     authenticateUser(req, res, next);
     expect(next).toHaveBeenCalled();
+  });
+
+  it("returns 403 error if they don't match", () => {
+    const req = {
+      cookies: { user_sid: 1 },
+      headers: { Authorization: "Token 2" },
+    };
+    authenticateUser(req, res, next);
+    expect(res.sendStatus).toHaveBeenCalledWith(403);
+  });
+
+  it("returns 403 error if no Authorization header was provided", () => {
+    const req = {
+      cookies: { user_sid: 1 },
+      headers: {},
+    };
+    authenticateUser(req, res, next);
+    expect(res.sendStatus).toHaveBeenCalledWith(403);
   });
 });
