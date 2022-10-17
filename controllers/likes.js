9696c954c068252207af4d3a6a404d4aa05afd6c
Zac <zac@zacmoss.co.uk>
2022-10-17 12:40:34 +0100

    update sends a 403 status if it throws an error while saving a like

--- controllers/likes.js
+++ controllers/likes.js
@@ -1,12 +1,16 @@
 exports.update = async (req, res, peepModel, likeModel) => {
-  const like = await new likeModel({
-    userId: req.session.user._id,
-    peepId: req.params.peepId,
-    username: req.session.user.username,
-  }).save();
-  await peepModel.findOneAndUpdate(
-    { _id: req.params.peepId },
-    { $push: { likes: like._id } }
-  );
-  res.json({ like: like });
+  try {
+    const like = await new likeModel({
+      userId: req.session.user._id,
+      peepId: req.params.peepId,
+      username: req.session.user.username,
+    }).save();
+    await peepModel.findOneAndUpdate(
+      { _id: req.params.peepId },
+      { $push: { likes: like._id } }
+    );
+    res.json({ like: like });
+  } catch {
+    res.sendStatus(403);
+  }
 };
--- test/controllers/likes.test.js
+++ test/controllers/likes.test.js
@@ -3,7 +3,7 @@ import * as likesController from "../../controllers/likes";
 let res;
 describe("Likes controller", () => {
   beforeEach(() => {
-    res = { json: jest.fn() };
+    res = { json: jest.fn(), sendStatus: jest.fn() };
   });
   it("update adds a like", async () => {
     const req = {
@@ -28,5 +28,18 @@ describe("Likes controller", () => {
     expect(res.json).toHaveBeenCalledWith({
       like: { _id: 1, userId: 1, peepId: 1, username: "foo" },
     });
+  });
+
+  it("update doesn't add a like if the peepId and userId already exist", async () => {
+    const req = {
+      params: { peepId: 1 },
+      session: { user: { _id: 1, username: "foo" } },
+    };
+    const mockPeepModel = { findOneAndUpdate: jest.fn() };
+    const mockLikeModel = jest.fn().mockImplementation(() => {
+      throw new Error("MongoServerError");
+    });
+    await likesController.update(req, res, mockPeepModel, mockLikeModel);
+    expect(res.sendStatus).toHaveBeenCalledWith(403);
   });
 });
