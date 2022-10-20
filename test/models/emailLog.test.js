9c5845bcd22be1efff813dca9b29c4c217113fd4
Zac <zac@zacmoss.co.uk>
2022-10-20 18:02:43 +0100

    emailLog can't save two entries with the same peepId and userId

--- test/models/emailLog.test.js
+++ test/models/emailLog.test.js
@@ -23,4 +23,42 @@ describe("Like model", () => {
     expect(emailLog.createdAt).toEqual(new Date(2022, 10, 20));
     expect(emailLog.successful).toBe(true);
   });
+
+  it("saves", async () => {
+    const emailLog = await new EmailLog({
+      peepId: mockPeepId,
+      userId: mockUserId,
+      createdAt: new Date(2022, 10, 20),
+      successful: true,
+    }).save();
+    expect(emailLog.peepId).toBe(mockPeepId);
+    expect(emailLog.userId).toBe(mockUserId);
+    expect(emailLog.createdAt).toEqual(new Date(2022, 10, 20));
+    expect(emailLog.successful).toBe(true);
+  });
+
+  it("can return all entries", async () => {
+    const result = await EmailLog.find();
+    expect(result).toEqual([]);
+  });
+
+  it("can't save two entries with the same peepId and userId", async () => {
+    expect.assertions(1);
+    await new EmailLog({
+      peepId: mockPeepId,
+      userId: mockUserId,
+      createdAt: new Date(2022, 10, 20),
+      successful: true,
+    }).save();
+    try {
+      await new EmailLog({
+        peepId: mockPeepId,
+        userId: mockUserId,
+        createdAt: new Date(2022, 10, 20),
+        successful: true,
+      }).save();
+    } catch (result) {
+      expect(result.toString()).toMatch(/MongoServerError/);
+    }
+  });
 });
