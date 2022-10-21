9d49f155c38393adb6b724b1ab849962369b56a7
Zac <zac@zacmoss.co.uk>
2022-10-21 09:03:54 +0100

    refactored createSession method

--- controllers/session.js
+++ controllers/session.js
@@ -10,16 +10,11 @@ exports.create = async (req, res) => {
 };
 
 const createSession = async (req, res) => {
-  const user = await User.findOne({
-    username: req.body.session.username,
-  });
+  const user = await User.findOne({ username: req.body.session.username });
   const result = await bcrypt.compare(req.body.session.password, user.password);
   if (!result) throw new Error("Password is wrong");
   req.session.user = user;
-  return res.status(201).json({
-    _id: user._id,
-    username: user.username,
-  });
+  return res.status(201).json({ _id: user._id, username: user.username });
 };
 
 exports.destroy = (req, res) => {
